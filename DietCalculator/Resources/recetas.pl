326e8d16ef19a4f5b49a7d349a17f7aa0b8780a7
SaulFeliciano02 <40696120+SaulFeliciano02@users.noreply.github.com>
2020-08-12 14:54:07 -0400

    archivo pl fix 2

--- DietCalculator/Resources/recetas.pl
+++ DietCalculator/Resources/recetas.pl
@@ -1,53 +1,17 @@
-cargar(A):-exists_file(A),consult(A).
-
 existe(X,[X|_]).
 existe(X,[_|Cola]):-existe(X,Cola).
 
-existeEnLista([],_).
-existeEnLista([H|T],L) :- existeEnLista(T,L), existe(H,L).
+sonParteDeReceta(List1,Resultado) :- ingredientes(Resultado,List2), forall(member(Element,List1), member(Element,List2)).
 
-sonParteDeReceta(List1,Resultado) :-
-    ingredientes(Resultado,List2),
-    forall(member(Element,List1),
-           member(Element,List2)).
+poseeUnIngrediente(Ingrediente,Resultado) :- ingredientes(Resultado,L), existe(Ingrediente,L).
 
-noExisteEnLista([],_).
-noExisteEnLista([H|T],L) :- noExisteEnLista(T,L), not(existe(H,L)).
+poseeTresIngredientes(X,Resultado) :- ingredientes(Resultado,L), existeEnLista(L,X).
 
-%%%% P R I M E R   P U N T O
-poseeUnIngrediente(Ingrediente,Resultado) 
-  :- ingredientes(Resultado,L),
-     existe(Ingrediente,L).
+poseeHerramienta(Herramienta,Resultado) :- herramientas(Resultado,L), existe(Herramienta,L).
 
-%%%% S E G U N D O   P U N T O
-poseeTresIngredientes(X,Resultado) 
-  :- ingredientes(Resultado,L),
-     existeEnLista(L,X).
+noPoseeHerramienta(Herramienta,Resultado) :- herramientas(Resultado,L), not(existe(Herramienta,L)).
 
-%%%% T E R C E R  P U N T O
-poseeHerramienta(Herramienta,Resultado) 
-  :- herramientas(Resultado,L),
-     existe(Herramienta,L).
+noPoseeIngrediente(Ingrediente,Resultado) :- ingredientes(Resultado,L), not(existe(Ingrediente,L)).
 
-%%%% C U A R T O  P U N T O
-noPoseeHerramienta(Herramienta,Resultado) 
-  :- herramientas(Resultado,L),
-     not(existe(Herramienta,L)).
+noPoseeIngredienteHerramienta(Ingrediente, Herramienta,Resultado) :- ingredientes(Resultado,L), not(existe(Ingrediente,L)), herramientas(Resultado,L2), not(existe(Herramienta,L2)).
 
-%%%% Q U I N T O  P U N T O
-noPoseeIngrediente(Ingrediente,Resultado) 
-  :- ingredientes(Resultado,L),
-     not(existe(Ingrediente,L)).
-
-%%%% S E X T O  P U N T O
-noPoseeIngredienteHerramienta(Ingrediente, Herramienta,Resultado)
-  :- ingredientes(Resultado,L),
-     not(existe(Ingrediente,L)),
-     herramientas(Resultado,L2),
-     not(existe(Herramienta,L2)).
-
-%%%% E X T R A
-noPoseeMultiplesIngredientes(X) 
-  :- ingredientes(Resultado,L),
-     noExisteEnLista(L,X),
-     write(Resultado),nl,fail.
